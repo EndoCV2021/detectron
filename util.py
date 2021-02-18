cd00de33c6d12519eb3fa46cae68b2efea425803
hyunseoki <hyunseoki@kaist.ac.kr>
2021-02-18 18:02:46 +0900

    디텍트론 내용 추가

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import torch
 
+
 def seed_everything(seed):
     random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
@@ -17,3 +18,5 @@ def seed_everything(seed):
     
     torch.backends.cudnn.deterministic = True ## cuda.manual_seed 안에 포함된다고 함. 하지만, 찜찜해서 또 넣음
     torch.backends.cudnn.benchmark = False
+
+    ## 이 외에도 cfg.SEED = 3 이런식으로 넣어줘야 한다. 기본값이 -1(음수면 랜덤 활성화)이라 넣어줘야함
